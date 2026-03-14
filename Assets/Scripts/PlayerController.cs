b7201415746e6c9836e8290eab4c3b46c874b815
wishMeLuck9 <149300259+wishMeLuck9@users.noreply.github.com>
2026-03-14 23:29:21 +0000

    per

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f;
+        transform.position = position;
     }
 }
