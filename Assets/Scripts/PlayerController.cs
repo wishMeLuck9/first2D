146d033cd422b0b525a89d32bdc440ff24fe7259
wishMeLuck9 <149300259+wishMeLuck9@users.noreply.github.com>
2026-05-08 11:42:20 +0100

    2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,8 +14,38 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var keyboard = Keyboard.current;
+        if (keyboard == null)
+        {
+            return;
+        }
+
+        float horizontal = 0.0f;
+        if (keyboard.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f;
+        }
+        else if (keyboard.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+
+        float vertical = 0.0f;
+        if (keyboard.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
+        else if (keyboard.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+
+        Debug.Log(horizontal);
+
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertical;
         transform.position = position;
     }
 }
