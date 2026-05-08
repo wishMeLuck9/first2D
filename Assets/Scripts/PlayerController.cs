542d76746f71f7378f02807d9942544b1b1095c4
wishMeLuck9 <149300259+wishMeLuck9@users.noreply.github.com>
2026-05-08 11:53:34 +0100

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction LeftAction;
+
     // Start is called before the first frame update
     void Start()
     {
